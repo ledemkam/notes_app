1524dcebb5928450a4d87bbb9a9dcb888d045a21
ledemkam <81742990+ledemkam@users.noreply.github.com>
2024-08-15 15:29:23 +0200

    Update DisplayedNote.tsx

--- client/src/pages/DisplayedNote.tsx
+++ client/src/pages/DisplayedNote.tsx
@@ -1,34 +1,20 @@
 import { Link, useParams } from 'react-router-dom';
-import { useDeleteNote, useGetNotes, useUpdateNote } from '../lib/react-query/query';
+import { useDeleteNote, useGetNotes} from '../lib/react-query/query';
 import { toast } from 'sonner';
 
 
 export default function DisplayedNote() {
     const { id } = useParams();
     const { data: notes } = useGetNotes();
-    const { mutateAsync: updateNote } = useUpdateNote();
+
     const { mutateAsync: deleteOneNote } = useDeleteNote();
 
     const aktuelNote = notes?.find((note) => note.id === Number(id));
 
-    const handleUpdate = async () => {
-            if (!aktuelNote) {
-                toast.error('Note not found');
-                return;
-            }
-            try {
-                await updateNote({
-                    id: aktuelNote.id,
-                    title: aktuelNote.title,
-                    subtitle: aktuelNote.subtitle,
-                    body_text: aktuelNote.body_text,
-                });
-                toast.success('Note updated successfully');
-            } catch {
-
-                toast.error('Failed to update note');
-            }
-    };
+   // if (!aktuelNote) {
+    //    toast.error('Note not found');
+      //  return;
+    //}
 
     const handleDelete = async (id:number) => {
      const noteTodelete = notes?.find((note) => note.id === id);
@@ -50,7 +36,6 @@ export default function DisplayedNote() {
             <Link
                 to={`/editer/${id}`}
                 className="px-2 py-1 text-slate-200 bg-green-600 rounded mr-2"
-                onClick={handleUpdate}
             >
                 Ändern
             </Link>
